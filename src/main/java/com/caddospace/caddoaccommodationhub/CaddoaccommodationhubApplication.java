1f3d83cade79c8074cfa2d81a6f57010438a9ba8
golam95 <golamnobi280@gmail.com>
2025-11-28 20:06:45 +0600

    first commit

---
+++ src/main/java/com/caddospace/caddoaccommodationhub/CaddoaccommodationhubApplication.java
@@ -0,0 +1,13 @@
+package com.caddospace.caddoaccommodationhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaddoaccommodationhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaddoaccommodationhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caddospace/caddoaccommodationhub/CaddoaccommodationhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caddospace.caddoaccommodationhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaddoaccommodationhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
